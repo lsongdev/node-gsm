825871a2aeecc60d5501f42adf29dc63953c3822
Lsong <song940@gmail.com>
2018-07-03 11:52:12 +0800

    add encoding options to modem#send_sms, refs to: #3

--- modem.js
+++ modem.js
@@ -2,6 +2,7 @@
 const util         = require('util');
 const async        = require('async');
 const EventEmitter = require('events');
+const iconv        = require('iconv-lite');
 const SerialPort   = require('serialport');
 const PDU          = require('./pdu');
 /**
@@ -182,7 +183,8 @@ Modem.prototype.sms_read = function(index){
   return this.set('CMGR', index);
 }
 
-Modem.prototype.sms_send = function(number, content) {
+Modem.prototype.sms_send = function(number, content, encoding) {
+  content = iconv.encode(content, encoding || this.encoding)
   // temporary disable retry.
   return this.set('CMGS', `"${number}"`, { retry: 0 }).then(x => {
     return this.send(content + '\u001a');
